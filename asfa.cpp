26dfcaa2674ba9369496905e042e55ffd702138a
mayank <46345959+mayank171@users.noreply.github.com>
2020-07-15 17:35:51 +0530

    Initial Commit
    Explanation of Code 
    
    1. First we will write a simple code of linked list which will have 2 functions:
    
         - insert() - which will insert elements in the linked list.
    
         - disp() - which will display the linked list.
    
    2. Insert some values in the linked list.
    
    3. Now we have created a function named sort() for sorting of linked list in increasing order.
    
    4. Then we have created 'temp' which is a pointer to node. It is initialized to head .
    
    5.  Now we have started a while loop for iterating the list. Remember we are iterating from first node till the second last node.
    
         while(temp->next!=NULL)
    
    6. Then we have created a new 'temp1' which is another pointer to node. It is initialized to temp->next which means it will always point to the next node of temp .
    
        struct node* temp1=temp->next;
    
    7. Now we are starting another loop from temp1 till the last node.
    
        while(temp1!=NULL)
    8. Then we are comparing the data of 2 nodes adjacent to each other. If data of previous node is greater than the data of node ahead of it the  we will swap the 2 nodes.
    
        if(temp->data > temp1->data)
       {
            swap(temp->data,temp1->data);
       }
    
    9. Then we have incremented temp1 so that it can point to a new node.
    
       temp1=temp1->next;
    
    10. This loop will run until a node comes to its original position in a sorted list.
    
    11. Then outside this loop we have incremented temp.
    
        temp=temp->next;

---
+++ asfa.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next;
+};
+struct node* head=NULL;
+
+class linked
+{
+public:
+    void insert(int x)
+    {
+        struct node* temp=new node();
+        temp->data=x;
+        temp->next=NULL;
+        if(head==NULL)
+        {
+            head=temp;
+        }
+        else
+        {
+            struct node* temp1=head;
+            while(temp1->next!=NULL)
+            {
+                temp1=temp1->next;
+            }
+            temp1->next =temp;
+
+        }
+    }
+
+
+    void sort()
+    {
+        struct node* temp=head;
+
+        while(temp->next!=NULL)
+        {
+            struct node* temp1=temp->next;
+            while(temp1!=NULL)
+            {
+                if(temp->data > temp1->data)
+                {
+                    swap(temp->data,temp1->data);
+                }
+                temp1=temp1->next;
+            }
+            temp=temp->next;
+        }
+    }
+
+
+    void disp()
+    {
+        struct node* temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    linked l;
+    l.insert(2);
+    l.insert(5);
+    l.insert(21);
+    l.insert(46);
+    l.insert(48);
+    l.insert(54);
+    l.insert(10);
+    l.insert(6);
+    l.insert(18);
+    l.insert(-84);
+    l.insert(90);
+    l.insert(42);
+    l.insert(81);
+    l.insert(34);
+    l.insert(-101);
+    l.insert(6);
+    l.insert(48);
+    l.insert(54);
+    l.insert(5);
+    l.insert(-6);
+    l.insert(8);
+    l.insert(-54);
+    l.insert(10);
+
+    l.disp();
+    l.sort();
+    l.disp();
+    return 0;
+}
+
+
